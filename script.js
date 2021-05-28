c222ae12ac3cae0b82d4721b6cc261f3d7ed9e0c
vinicyusx <vinicyus3029@gmail.com>
2021-05-28 19:35:25 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,14 @@
+for(let v= 1;v <=10;v++){
+    console.log(v)
+}
+let teste= 10;
+while(teste>=1){
+    console.log(teste)
+    teste--
+}
+for(let a=0;a<=100;a+=3){
+    console.log(a)
+}
+for(let b=0;b<=100;b+=2){
+    console.log(b)
+}
