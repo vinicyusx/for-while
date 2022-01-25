e7784a16824faf800809743b4dbf9a07e0bc9268
vinicyusx <vinicyus3029@gmail.com>
2022-01-25 18:10:26 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ while(teste>=1){
     console.log(teste)
     teste--
 }
-for(let a=0;a<=100;a+=3){
+for(let a=1;a<=100;a+=2){
     console.log(a)
-}
-for(let b=0;b<=100;b+=2){
-    console.log(b)
 }
